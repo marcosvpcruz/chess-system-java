52599ef28e0bea27538291fddd44139e2dc228d6
Marcos Vinicius <marcosvpcruz@yahoo.com.br>
2020-03-23 13:12:50 -0300

    Implementação Board e Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
@@ -8,9 +8,7 @@ public class Program {
 		// TODO Auto-generated method stub
 
 		
-		Position pos = new Position(3,5);
-		
-		System.out.println(pos);
+		Board board = new Board(8,8);
 	}
 
 }
--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -3,6 +3,7 @@ package boardgame;
 public class Position {
 	private int row;
 	private int colunm;
+	
 	public Position(int row, int colunm) {
 		this.row = row;
 		this.colunm = colunm;
